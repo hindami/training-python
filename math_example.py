f2e5bac02c62e3450f1b21e4028de73d5ed80dfa
hindami <hindami050294@gmail.com>
2025-07-21 11:07:07 +0700

    add math native

---
+++ math_example.py
@@ -0,0 +1,31 @@
+import math
+
+# built in math func
+
+
+x = min(5, 10, 25)  # find the min value
+y = max(5, 10, 25)  # find the max value
+
+print(x)
+print(y)
+
+z = abs(-7.25)  # return the absolute positive value
+print(z)
+
+d = pow(4, 3)  # it means 4 * 4 * 4
+print(d)
+
+# the math module
+
+ux = math.sqrt(64)  # get square route
+print(ux)
+
+
+uy = math.ceil(1.4)  # rounded upward to the nearest integer
+uz = math.floor(1.4)  # rounded downward to the nearest integer
+uo = math.pi  # 3.14444444
+
+
+print(uy)
+print(uz)
+print(uo)
