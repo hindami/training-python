f6c30c445e5a44a4006ad610329e572964f280a2
hindami <hindami050294@gmail.com>
2025-07-23 08:55:54 +0700

    add tokenizer

--- tokenizer.py
+++ tokenizer.py
@@ -1,5 +1,9 @@
 import tiktoken
 
+from tokenizers import Tokenizer
+
+tokenizer = Tokenizer.from_pretrained("bert-base-cased")
+
 enc = tiktoken.get_encoding("o200k_base")
 # assert enc.decode(enc.encode("hello world")) == "hello world"
 
